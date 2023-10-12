388b829f190e26b961d8b91cca917ad1f411e768
Ignacio Gutierrez <nachogutierrezibanez@gmail.com>
2023-10-12 21:39:57 +0200

    reimplement client-side cache

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,22 +1,52 @@
+import { LRUCache, serialize, deserialize } from "./lru.js"
+
+function loadCache() {
+    let serializedLru = localStorage.getItem('api_responses')
+    if (serializedLru !== null) {
+        return deserialize(serializedLru)
+    } else {
+        return new LRUCache(500)
+    }
+}
+
+function saveCache(cache) {
+    localStorage.setItem('api_responses', serialize(cache))
+}
+
 export async function getApi() {
     const config = await (await fetch('config.json')).json()
     const { apiEndpoint } = config
     return API(apiEndpoint)
 }
 
-// TODO: implement caching using LRU
 function API(endpoint) {
 
     async function search(query) {
+        let cache = loadCache()
+        const cachedResponse = cache.get(query)
+        if (!(cachedResponse < 0)) {
+            console.debug(`search("${query}") cache hit`)
+            return cachedResponse
+        }
+        console.debug(`search("${query}") cache miss`)
+
         const url = `${endpoint}/search?query=${encodeURIComponent(query)}`
         const response = await fetch(url)
-        return response.json()
+        const wordList = await response.json()
+
+        cache = loadCache()
+        cache.put(query, wordList)
+        saveCache(cache)
+        return wordList
     }
 
     async function get(query) {
-        const url = `${endpoint}/get?query=${encodeURIComponent(query)}`
-        const response = await fetch(url)
-        return response.json()
+        const response = await search(query)
+        const word = response.find(x => x.key.toLowerCase() === query.toLowerCase())
+        if (!word) {
+            throw new Error(`Word not found: ${query}`)
+        }
+        return word
     }
 
     return {
